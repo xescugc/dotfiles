57e335e27895a1483e2c2f09018fa28a656d9659
xescuGC <xescugil@gmail.com>
2017-04-17 02:01:35 +0200

    Ident with spaces and used the xbanish

--- installa.sh
+++ installa.sh
@@ -16,20 +16,26 @@ echo ""
 
 if confirm "Installing needed packages?"
 then
-	sudo pacman -S i3 xorg xorg-xinit i3status rofi rxvt-unicode i3lock openssh ranger vim
+  sudo pacman -S i3 xorg xorg-xinit i3status rofi rxvt-unicode i3lock \
+    openssh ranger vim
 
-	yaourt -S gohufont
+  yaourt -S gohufont xbanish
 fi
 
 if confirm "Create symlinks for the configuration?"
 then
-	ln -svi -T $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
-	ln -svi -T $HOME/dotfiles/X/.Xdefaults $HOME/.Xdefaults
-	ln -svi -T $HOME/dotfiles/X/.xinitrc $HOME/.xinitrc
-	ln -svi -T $HOME/dotfiles/system/.bashrc $HOME/.bashrc
-	ln -svi -T $HOME/dotfiles/system/.bash_profile $HOME/.bash_profile
-	ln -svi -T $HOME/dotfiles/i3/config $HOME/.config/i3/config
-	ln -svi -T $HOME/dotfiles/i3status/config $HOME/.config/i3status/config
-	ln -svi -T $HOME/dotfiles/rofi/config $HOME/.config/rofi/config
-	ln -svi $HOME/dotfiles/vim $HOME/.vim
+  ln -svi -T $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
+  ln -svi -T $HOME/dotfiles/X/.Xdefaults $HOME/.Xdefaults
+  ln -svi -T $HOME/dotfiles/X/.xinitrc $HOME/.xinitrc
+  ln -svi -T $HOME/dotfiles/system/.bashrc $HOME/.bashrc
+  ln -svi -T $HOME/dotfiles/system/.bash_profile $HOME/.bash_profile
+  ln -svi -T $HOME/dotfiles/i3/config $HOME/.config/i3/config
+  ln -svi -T $HOME/dotfiles/i3status/config $HOME/.config/i3status/config
+  ln -svi -T $HOME/dotfiles/rofi/config $HOME/.config/rofi/config
+  ln -svi $HOME/dotfiles/vim $HOME/.vim
+fi
+
+if confirm "Install Vim Plugins? (with Vundle)"
+then
+  vim +PluginInstall +qall
 fi
